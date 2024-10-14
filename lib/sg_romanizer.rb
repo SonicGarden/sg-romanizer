29c91f6ba14687c283733c7a952a750e4cffb6c0
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-10-14 20:47:54 +0900

    fix romanize

--- lib/sg_romanizer.rb
+++ lib/sg_romanizer.rb
@@ -1,48 +1,47 @@
 class SgRomanizer
   def romanize(arabic)
     # write your code here
-    if arabic == 1 then
-      return "I"
-    elsif arabic == 2 then
-      return "II"
-    elsif arabic == 3 then
-      return "III"
-    end
-
     str_arabic = arabic.to_s  
     len = str_arabic.length
-    
     result = ""
+    
     len.times do |i|
       target_digit = str_arabic[i].to_i
       if len - i == 4 then
         # 10^3
-        result += "M" * str_arabic[i].to_i
+        result += "M" * target_digit
       elsif len - i == 3 then
         # 10^2
-        if target_digit < 5 then
-          if target_digit < 4 then
-            result += "C" * target_digit
-          else
-            result += "CD"
-          end
-        elsif target_digit == 5 then
-          result += "D"
-        elsif 5 < target_digit && target_digit < 9 then
-          result += "D" + "C" * (target_digit - 5)
-        elsif target_digit == 9 then
-          result += "CM"
-        end
+        result += generateRoman(target_digit, "D", "C", "M")
       elsif len - i == 2 then
         # 10^1
+        result += generateRoman(target_digit, "L", "X", "C")
       elsif len - i == 1 then
         # 10^0
+        result += generateRoman(target_digit, "V", "I", "X")
       end
     end
 
     return result
   end
     
+  def generateRoman(target_digit, fifth_key, unit, nextUnit)
+    if target_digit < 5 then
+      if target_digit < 4 then
+        return unit * target_digit
+      else
+        return unit + fifth_key
+      end
+    elsif target_digit == 5 then
+      return fifth_key
+    elsif 5 < target_digit && target_digit < 9 then
+      return fifth_key + unit * (target_digit - 5)
+    elsif target_digit == 9 then
+      return unit + nextUnit
+    end
+    
+  end
+
   def deromanize(roman)
     # write your code here
     if roman == "I" then
--- test/sg_romanizer_test_small.rb
+++ test/sg_romanizer_test_small.rb
@@ -7,29 +7,63 @@ class SgRomanizerTest < Minitest::Test
     @romanizer = SgRomanizer.new
   end
 
-  def test_romanize
+  def test_romanize_1_to_10
     assert_equal 'I', @romanizer.romanize(1)
     assert_equal 'II', @romanizer.romanize(2)
     assert_equal 'III', @romanizer.romanize(3)
-    
+    assert_equal 'IV', @romanizer.romanize(4)
+    assert_equal 'V', @romanizer.romanize(5)
+    assert_equal 'VI', @romanizer.romanize(6)
+    assert_equal 'VII', @romanizer.romanize(7)
+    assert_equal 'VIII', @romanizer.romanize(8)
+    assert_equal 'IX', @romanizer.romanize(9)
+    assert_equal 'X', @romanizer.romanize(10)
+  end
+
+  def test_romanize_10_to_100
+    assert_equal 'X', @romanizer.romanize(10)
+    assert_equal 'XX', @romanizer.romanize(20)
+    assert_equal 'XXX', @romanizer.romanize(30)
+    assert_equal 'XL', @romanizer.romanize(40)
+    assert_equal 'L', @romanizer.romanize(50)
+    assert_equal 'LX', @romanizer.romanize(60)
+    assert_equal 'LXX', @romanizer.romanize(70)
+    assert_equal 'LXXX', @romanizer.romanize(80)
+    assert_equal 'XC', @romanizer.romanize(90)
+    assert_equal 'C', @romanizer.romanize(100)
+  end
+
+  def test_romanize_100_to_1000
+    assert_equal 'C', @romanizer.romanize(100)
+    assert_equal 'CC', @romanizer.romanize(200)
+    assert_equal 'CCC', @romanizer.romanize(300)
+    assert_equal 'CD', @romanizer.romanize(400)
+    assert_equal 'D', @romanizer.romanize(500)
+    assert_equal 'DC', @romanizer.romanize(600)
+    assert_equal 'DCC', @romanizer.romanize(700)
+    assert_equal 'DCCC', @romanizer.romanize(800)
+    assert_equal 'CM', @romanizer.romanize(900)
     assert_equal 'M', @romanizer.romanize(1000)
-    assert_equal 'MC', @romanizer.romanize(1100)
-    assert_equal 'MCC', @romanizer.romanize(1200)
-    assert_equal 'MCCC', @romanizer.romanize(1300)
-    assert_equal 'MCD', @romanizer.romanize(1400)
-    assert_equal 'MD', @romanizer.romanize(1500)
-    assert_equal 'MDC', @romanizer.romanize(1600)
-    assert_equal 'MDCC', @romanizer.romanize(1700)
-    assert_equal 'MDCCC', @romanizer.romanize(1800)
-    assert_equal 'MCM', @romanizer.romanize(1900)
+  end
+
+  def test_romanize_1000_to_3000
+    assert_equal 'M', @romanizer.romanize(1000)
     assert_equal 'MM', @romanizer.romanize(2000)
-    # assert_equal 'IV', @romanizer.romanize(4)
-    # assert_equal 'V', @romanizer.romanize(5)
-    # assert_equal 'VI', @romanizer.romanize(6)
-    # assert_equal 'VII', @romanizer.romanize(7)
-    # assert_equal 'VIII', @romanizer.romanize(8)
-    # assert_equal 'IX', @romanizer.romanize(9)
-    # assert_equal 'X', @romanizer.romanize(10)
+    assert_equal 'MMM', @romanizer.romanize(3000)
+  end
+
+  def test_romanize_493
+    assert_equal 'CDXCIII', @romanizer.romanize(493)
+  end
+
+  def test_romanize_2024
+    assert_equal 'MMXXIV', @romanizer.romanize(2024)
+  end
+
+  def test_romanize_all
+    load_pattern.each do |arabic, roman|
+      assert_equal roman, @romanizer.romanize(arabic)
+    end
   end
 
   def test_deromanize_1_to_10
