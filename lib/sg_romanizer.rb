9a98160f5d13da334793018929f2c867f68bc131
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-10-14 21:32:59 +0900

    add deromanize

--- lib/sg_romanizer.rb
+++ lib/sg_romanizer.rb
@@ -41,48 +41,13 @@ class SgRomanizer
   end
 
   def deromanize(roman)
-    # write your code here
-    if roman == "I" then
-      return 1
-    elsif roman == "II" then
-      return 2
-    elsif roman == "III" then
-      return 3 
+    4000.times do |i|
+      if roman == romanize(i) then
+        return i
+      end
     end
+    
+    return 0
   end
-
-  # 1: 'I'
-  # 2: 'II'
-  # 3: 'III'
-  # 4: 'IV'
-  # 5: 'V'
-  # 6: 'VI'
-  # 7: 'VII'
-  # 8: 'VIII'
-  # 9: 'IX'
-  # 10: 'X'
-  # 10 = X
-  # 20 = XX
-  # 30 = XXX
-  # 40 = XL
-  # 50 = L
-  # 60 = LX
-  # 70 = LXX
-  # 80 = LXXX
-  # 90 = XC
-  # 100 = C
-  # 200 = CC
-  # 300 = CCC
-  # 400 = CD
-  # 500 = D
-  # 600 = DC
-  # 700 = DCC
-  # 800 = DCCC
-  # 900 = CM
-  # 1000 = M
-  # 2000 = MM
-  # 3000 = MMM
   
 end
-
-sgr = SgRomanizer.new
