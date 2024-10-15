35548a1d48e7e7c2dddd2295dddeaf6c7e0f52e9
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-10-15 18:31:35 +0900

    とりあえず実装完了

--- lib/sg_romanizer.rb
+++ lib/sg_romanizer.rb
@@ -1,6 +1,5 @@
 class SgRomanizer
   def romanize(arabic)
-    # write your code here
     str_arabic = arabic.to_s  
     len = str_arabic.length
     result = ""
@@ -36,18 +35,87 @@ class SgRomanizer
       return fifth_key + unit * (target_digit - 5)
     elsif target_digit == 9 then
       return unit + nextUnit
+    else
+      return "Unexpected number."
     end
-    
   end
 
   def deromanize(roman)
-    4000.times do |i|
-      if roman == romanize(i) then
-        return i
-      end
+    target = roman
+    result = 0
+
+    while target.length != 0
+      arabic, removed_length = generateArabic(target)
+      result += arabic
+      target = target.slice(removed_length, target.length)
     end
-    
-    return 0
+    return result
+  end
+
+  def generateArabic(roman)
+    if roman.start_with?("MMM") then
+      return 3000, 3
+    elsif roman.start_with?("MM") then
+      return 2000, 2
+    elsif roman.start_with?("M") then
+      return 1000, 1
+    elsif roman.start_with?("CM") then
+      return 900, 2
+    elsif roman.start_with?("DCCC") then
+      return 800, 4
+    elsif roman.start_with?("DCC") then
+      return 700, 3
+    elsif roman.start_with?("DC") then
+      return 600, 2
+    elsif roman.start_with?("D") then
+      return 500, 1
+    elsif roman.start_with?("CD") then
+      return 400, 2
+    elsif roman.start_with?("CCC") then
+      return 300, 3
+    elsif roman.start_with?("CC") then
+      return 200, 2
+    elsif roman.start_with?("C") then
+      return 100, 1
+    elsif roman.start_with?("XC") then
+      return 90, 2
+    elsif roman.start_with?("LXXX") then
+      return 80, 4
+    elsif roman.start_with?("LXX") then
+      return 70, 3
+    elsif roman.start_with?("LX") then
+      return 60, 2
+    elsif roman.start_with?("L") then
+      return 50, 1
+    elsif roman.start_with?("XL") then
+      return 40, 2
+    elsif roman.start_with?("XXX") then
+      return 30, 3
+    elsif roman.start_with?("XX") then
+      return 20, 2
+    elsif roman.start_with?("X") then
+      return 10, 1
+    elsif roman.start_with?("IX") then
+      return 9, 2
+    elsif roman.start_with?("VIII") then
+      return 8, 4
+    elsif roman.start_with?("VII") then
+      return 7, 3
+    elsif roman.start_with?("VI") then
+      return 6, 2
+    elsif roman.start_with?("V") then
+      return 5, 1
+    elsif roman.start_with?("IV") then
+      return 4, 2
+    elsif roman.start_with?("III") then
+      return 3, 3
+    elsif roman.start_with?("II") then
+      return 2, 2
+    elsif roman.start_with?("I") then
+      return 1, 1
+    else
+      return 0, 0
+    end
   end
   
 end
