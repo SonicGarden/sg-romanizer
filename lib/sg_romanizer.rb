2b8fc1d48e7fdd26eb550a614ab81fa5683948dc
Takashi Miyamoto <miyamoto.t@castlehotel.co.jp>
2024-10-17 15:33:38 +0900

    ローマ数字を1文字ずつ処理する方法に変更

--- lib/sg_romanizer.rb
+++ lib/sg_romanizer.rb
@@ -11,16 +11,10 @@ class SgRomanizer
   end
 
   def deromanize(roman)
-    {
-      "MMM"=> 3000, "MM"=> 2000, "M"=> 1000,
-      "CM"=> 900, "DCCC"=> 800, "DCC"=> 700, "DC"=> 600, "D"=> 500, "CD"=> 400, "CCC"=> 300, "CC"=> 200, "C"=> 100,
-      "XC"=> 90, "LXXX"=> 80, "LXX"=> 70, "LX"=> 60, "L"=> 50, "XL"=> 40, "XXX"=> 30, "XX"=> 20, "X"=> 10,
-      "IX"=> 9, "VIII"=> 8, "VII"=> 7, "VI"=> 6, "V"=> 5, "IV"=> 4, "III"=> 3, "II"=> 2, "I"=> 1
-    }.map do |k,v|
-      if roman.index(k) == 0 then
-        roman = roman[k.length..]
-        v
-      end
-    end.compact.sum
+    roman_chars = roman.chars
+    roman_chars.map.with_index do |r,i|
+      arabic = {"I"=> 1, "V"=> 5, "X"=> 10, "L"=> 50, "C"=> 100, "D"=> 500, "M"=> 1000}[r]
+      (arabic < {"I"=> 1, "V"=> 5, "X"=> 10, "L"=> 50, "C"=> 100, "D"=> 500, "M"=> 1000}[roman_chars[i + 1]].to_i ? -1 : 1) * arabic
+    end.sum
   end
 end
