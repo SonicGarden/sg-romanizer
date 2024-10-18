02e01eaeb5827917dabdeb5cc377e7549672c8b2
Takaho Takemura <jp.takapon@gmail.com>
2024-10-18 16:20:30 +0900

    リファクタ

--- lib/sg_romanizer.rb
+++ lib/sg_romanizer.rb
@@ -1,65 +1,55 @@
 class SgRomanizer
   ROMAN_WEIGHTS = {
     "I" => 1,
+    "IV" => 4,
     "V" => 5,
+    "IX" => 9,
     "X" => 10,
+    "XL" => 40,
     "L" => 50,
+    "XC" => 90,
     "C" => 100,
+    "CD" => 400,
     "D" => 500,
+    "CM" => 900,
     "M" => 1000
   }.freeze
   ROMAN_CHAR = ROMAN_WEIGHTS.invert.freeze
 
-  def initialize
-
-  end
-
   def romanize(arabic)
     ret = ""
-    nums = [1000, 100, 10]
-    nums.each do |num|
-      nine = (num * 0.9).to_i
-      five = (num * 0.5).to_i
-      four = (num * 0.4).to_i
-      one = (num * 0.1).to_i
 
-      ret += ROMAN_CHAR[num] * (arabic / num)
-      arabic %= num
+    [1000, 100, 10].each do |num|
+      portions = [
+        num,
+        (num * 0.9).to_i,
+        (num * 0.5).to_i,
+        (num * 0.4).to_i,
+        (num * 0.1).to_i
+      ]
 
-      ret += (ROMAN_CHAR[one] + ROMAN_CHAR[num]) * (arabic / nine)
-      arabic %= nine
-
-      ret += ROMAN_CHAR[five] * (arabic / five)
-      arabic %= five
-
-      ret += (ROMAN_CHAR[one] + ROMAN_CHAR[five]) * (arabic / four)
-      arabic %= four
-
-      ret += ROMAN_CHAR[one] * (arabic / one)
-      arabic %= one
+      portions.each do |portion|
+        ret += ROMAN_CHAR[portion] * (arabic / portion)
+        arabic %= portion
+      end
     end
+
     ret
   end
 
   def deromanize(roman)
     ret = 0
-    should_skip_next = false
-    roman.chars.each_cons(2) do |cur_char, next_char|
-      if should_skip_next
-        should_skip_next = false
-        next
-      end
 
-      if cur_char == next_char
-        ret += ROMAN_WEIGHTS[cur_char]
-      elsif ROMAN_WEIGHTS[cur_char] < ROMAN_WEIGHTS[next_char]
-        ret += ROMAN_WEIGHTS[next_char] - ROMAN_WEIGHTS[cur_char]
-        should_skip_next = true
+    while !roman.empty?
+      if ROMAN_WEIGHTS[roman[0..1]]
+        ret += ROMAN_WEIGHTS[roman[0..1]]
+        roman.slice!(0, 2)
       else
-        ret += ROMAN_WEIGHTS[cur_char]
+        ret += ROMAN_WEIGHTS[roman[0]]
+        roman.slice!(0, 1)
       end
     end
-    ret += ROMAN_WEIGHTS[roman[-1]] unless should_skip_next
+
     ret
   end
 end
